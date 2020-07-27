b89b0a99d2fc14f837b05414db1ef48d503e0703
ericma <924402086@qq.com>
2020-07-27 16:53:06 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # maven_demo
 maven demo
+
+compile   编译
+test      测试
+package   打包
+clean     清除编译的代码
+install   把项目作为本地仓库的代码
---
+++ src/main/java/com/imooc/maven01/model/Hello.java
@@ -0,0 +1,7 @@
+package com.imooc.maven01.model;
+
+public class Hello {
+  public String sayHello(){
+    return "hello world";
+  }
+}
